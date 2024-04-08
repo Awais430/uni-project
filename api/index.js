bdcc39e5978903754b6e9a08ec0e1fc9e2bec335
Awais430 <hamzaaliics@gmail.com>
2024-04-08 22:11:39 +0500

    last commint for index.js

--- api/index.js
+++ api/index.js
@@ -43,10 +43,10 @@ app.use("/api/v1", comment);
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'public', 'index.html'));
 });
 
 
